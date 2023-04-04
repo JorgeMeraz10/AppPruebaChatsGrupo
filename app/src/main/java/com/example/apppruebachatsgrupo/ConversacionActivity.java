1fe275b13568607ba05a3eab9267578001a0fe7c
MASTERALD <alexmeraz4567@gmail.com>
2023-04-03 23:24:56 -0600

    Actualizacion 03/04/2023

--- app/src/main/java/com/example/apppruebachatsgrupo/ConversacionActivity.java
+++ app/src/main/java/com/example/apppruebachatsgrupo/ConversacionActivity.java
@@ -2,12 +2,14 @@ package com.example.apppruebachatsgrupo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import java.util.ArrayList;
 
@@ -41,8 +43,6 @@ public class ConversacionActivity extends AppCompatActivity {
                 "                      Proyecto de Programacion Movil I.");
         mChatMessages.add("Estefany:   Si esta muy largo, hay que investigar mucho.");
 
-
-
         // Inicializa el adaptador de la lista de mensajes del chat
         mChatListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mChatMessages);
 
@@ -63,7 +63,40 @@ public class ConversacionActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Vincula el método showAttachmentOptions con el botón de adjuntar
+        findViewById(R.id.attach_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAttachmentOptions(v);
+            }
+        });
     }
 
-
+    public void showAttachmentOptions(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.getMenuInflater().inflate(R.menu.attachment_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_image:
+                        // Handle image option
+                        return true;
+                    case R.id.menu_document:
+                        // Handle document option
+                        return true;
+                    case R.id.menu_audio:
+                        // Handle audio option
+                        return true;
+                    case R.id.menu_video:
+                        // Handle video option
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popup.show(); // muestra el menú emergente
+    }
 }
